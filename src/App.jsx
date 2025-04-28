3b66cbffc516adf4b0bda68853b4acd6db855082
User <user@example.com>
2025-04-28 09:24:01 -0700

    added files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ComplexNumbers from './components/ComplexNumbers'
+
+
+function App() {
+  return (
+      <div className="App">
+        <ComplexNumbers />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/ComplexNumbers.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const ComplexNumbers = () => {
+
+
+	return (
+        <div></div>
+)
+};
+
+
+export default ComplexNumbers;
